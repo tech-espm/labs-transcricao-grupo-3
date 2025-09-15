f026068eddafa95b3573702629c90bb571214a82
PabloDimitrof <pablodimitrof@gmail.com>
2025-09-15 15:15:47 -0300

    criação do json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 from config import api_key
+import json
 
 client = OpenAI(api_key=api_key)
 audio_file= open("audioaqui/audio2.ogg", "rb") 
@@ -15,7 +16,13 @@ for w in (transcription.words or []):
     print(f"\t {(w.start * 1000):.0f}\t {(w.end * 1000):.0f}\t {w.word}")
 
 print(f"\n A transcrição completa do áudio é: {transcription.text} ")
-
-
-
-
+ 
+with open("teste.json", "w", encoding="utf-8") as arquivo_json:
+	lista = []
+	for word in transcription.words:
+		lista.append({
+			"start": word.start,
+			"end": word.end,
+			"word": word.word
+		})
+	arquivo_json.write(json.dumps(lista))
